de64b4d779384f7d0f5442010632205be1be3111
Javier Palomares <javier.palomares.90@gmail.com>
2018-06-10 15:08:07 -0500

    Initial checkin of rep ok tester. Still need to finish it

---
+++ src/pset1/SLListRepOkTester.java
@@ -0,0 +1,25 @@
+package pset1;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import pset1.SLList.Node;
+
+/**
+ * @author Javier Palomares
+ */
+public class SLListRepOkTester
+{
+    @Test public void t0()
+    {
+        SLList l = new SLList();
+        assertTrue(l.repOk());
+    }
+
+    @Test public void t1()
+    {
+        SLList l = new SLList();
+        Node n = new Node();
+        // your code goes here
+    }
+    // your code goes here
+}
