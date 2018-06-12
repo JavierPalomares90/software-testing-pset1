269945bd4873c2fb0410ccc1fdad2904a4b10127
Javier Palomares <javier.palomares.90@gmail.com>
2018-06-11 21:03:12 -0500

    Added 2 tests for repOk() for cyclic/acyclic. Not sure if this is what needs to be done

--- src/pset1/SLListRepOkTester.java
+++ src/pset1/SLListRepOkTester.java
@@ -15,11 +15,42 @@ public class SLListRepOkTester
         assertTrue(l.repOk());
     }
 
+    /**
+     * Test that an acylic test is repOk()
+     */
     @Test public void t1()
     {
         SLList l = new SLList();
         Node n = new Node();
         // your code goes here
+        // Initialize a new node that points to null
+        n.elem = true;
+        n.next = null;
+        // Initialize a new node that points to the node we just initialized
+        Node m = new Node();
+        m.elem = false;
+        m.next = n;
+        l.header = m;
+        assertTrue(l.repOk());
+    }
+
+    /**
+     * Test that an cylic test is not repOk()
+     */
+    @Test public void t2()
+    {
+        SLList l = new SLList();
+        Node n = new Node();
+        // your code goes here
+        n.elem = true;
+        n.next = null;
+        Node m = new Node();
+        m.elem = false;
+        // Make the 2 nodes point at each other to form a cycle
+        m.next = n;
+        n.next = m;
+        l.header = m;
+        assertFalse(l.repOk());
     }
     // your code goes here
 }
